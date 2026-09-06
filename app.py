7be99cf109788291e5a86c24b67c43b22b7a6b82
Ralph Ryan Escabarte <rtescabarte1@up.edu.ph>
2026-09-07 00:29:00 +0800

    feat: add toggle task done route

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ def init_db():
 def index():
     db = get_db()
     tasks = db.execute("SELECT * FROM tasks ORDER BY created_at DESC").fetchall()
-    return "<br>".join([f"{row['title']}" for row in tasks])    
+    return "<br>".join([f"{row['title']} - {row['is_done']}" for row in tasks])    
 
 @app.route("/add", methods=["POST"])
 def add_task():
@@ -45,6 +45,13 @@ def add_task():
     db.commit()
     return redirect(url_for("index"))
 
+@app.route("/toggle/<int:task_id>", methods=["POST"])
+def toggle_task(task_id):
+    db = get_db()
+    db.execute("UPDATE tasks SET is_done = NOT is_done WHERE id = ?", (task_id,))
+    db.commit()
+    return redirect(url_for("index"))
+    
 if __name__ == "__main__":
     init_db()
     app.run(debug=True)
