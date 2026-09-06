14d6eb0bdfd8abd93ce103f6217dc7a9e6204a7c
Ralph Ryan Escabarte <rtescabarte1@up.edu.ph>
2026-09-06 23:26:16 +0800

    feat: add database connection helpers and init

--- app.py
+++ app.py
@@ -1,1 +1,32 @@
-# To Be Implemented Later
+import sqlite3 
+from pathlib import Path
+from flask import Flask, g, render_template, request, redirect, url_for
+
+BASE_DIR = Path(__file__).resolve().parent
+DATABASE = BASE_DIR / "database" / "todo.db"
+SCHEMA = BASE_DIR / "database" / "schema.sql"
+app = Flask(__name__)
+
+def get_db():
+    if 'db' not in g:
+        g.db = sqlite3.connect(DATABASE)
+        g.db.row_factory = sqlite3.Row
+    return g.db
+
+@app.teardown_appcontext
+def close_db(exception=None):
+    connection = g.pop("db", None)
+
+    if connection is not None:
+        connection.close()
+
+def init_db():
+    with app.app_context():
+        db = get_db()
+        with open(SCHEMA, "r") as f:
+            db.executescript(f.read())
+        db.commit()
+
+if __name__ == "__main__":
+    init_db()
+    app.run(debug=True)
